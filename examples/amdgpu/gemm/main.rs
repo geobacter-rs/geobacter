1f301330eaa132f7e737ee75fe46cd36c4b1f7d1
Richard Diamond <wichard@vitalitystudios.com>
2020-03-09 14:47:32 -0500

    Don't include kernel codegen time in the benchmark.

--- examples/amdgpu/gemm/main.rs
+++ examples/amdgpu/gemm/main.rs
@@ -563,6 +563,8 @@ pub fn main() {
   let queue = dev.create_single_queue2(None, group_size, private_size)
     .expect("HsaAmdGpuAccel::create_single_queue");
 
+  // ensure the invocation does block on this step:
+  invoc.compile().expect("kernel cross codegen");
   async_copy_signal.wait_for_zero(false).unwrap();
   println!("starting GPU gemm...");
 
