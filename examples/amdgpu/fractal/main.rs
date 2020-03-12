a079c83818f543038aac4c5a26696dcf19354a34
Richard Diamond <wichard@vitalitystudios.com>
2020-03-12 15:25:13 -0500

    Use f64 for kicks. Also, no color now.

--- examples/amdgpu/fractal/main.rs
+++ examples/amdgpu/fractal/main.rs
@@ -23,14 +23,14 @@ fn work(args: &Args) {
   let dispatch = dispatch_packet();
   let id_x = dispatch.global_id_x();
   let id_y = dispatch.global_id_y();
-  let id = f32x2::new(id_x as f32, id_y as f32);
-  let norm = (id + 0.5f32) * args.scale / args.image_size();
-  let c = (norm - 0.5f32) * 2.0f32 - f32x2::new(1.0f32, 0.0);
+  let id = f64x2::new(id_x as f64, id_y as f64);
+  let norm = (id + 0.5f64) / args.image_size();
+  let c = (norm - 0.5f64) * 2.0f64 - f64x2::new(1.0f64, 0.0);
 
   let c = (c.extract(0), c.extract(1));
 
-  let mut z = (0.0f32, 0.0f32);
-  let mut i = 0.0f32;
+  let mut z = (0.0f64, 0.0f64);
+  let mut i = 0.0f64;
 
   loop {
     z = (
@@ -38,7 +38,7 @@ fn work(args: &Args) {
       2.0 * z.0 * z.1 + c.1,
     );
 
-    if (z.0 * z.0 + z.1 * z.1) > (1 << 16) as f32 {
+    if (z.0 * z.0 + z.1 * z.1) > 4.0 {
       break;
     }
 
@@ -46,19 +46,8 @@ fn work(args: &Args) {
     if i >= 1.0 { break; }
   }
 
-  let zx = if z.0 > 1.0 {
-    1.0
-  } else {
-    z.0
-  };
-  let zy = if z.1 > 1.0 {
-    1.0
-  } else {
-    z.1
-  };
-
-  let mut write = f32x4::new(i, zx, zy, 1.0f32);
-  write *= u8::max_value() as f32;
+  let mut write = f64x4::new(i, i, i, 1.0f64);
+  write *= u8::max_value() as f64;
 
   args.write_pixel((id_x as _, id_y as _), write.cast());
 }
@@ -66,14 +55,13 @@ fn work(args: &Args) {
 #[derive(GeobacterDeps)]
 pub struct Args {
   image: *mut [Elem],
-  scale: f32,
 }
 impl Args {
-  pub fn image_size(&self) -> f32x2 {
-    let x = X_SIZE as f32;
-    let y = Y_SIZE as f32;
+  pub fn image_size(&self) -> f64x2 {
+    let x = X_SIZE as f64;
+    let y = Y_SIZE as f64;
 
-    f32x2::new(x, y)
+    f64x2::new(x, y)
   }
   pub fn write_pixel(&self, ix: (usize, usize),
                      px: Elem) {
@@ -133,7 +121,6 @@ pub fn main() {
 
   let args = Args {
     image: &mut frames[..],
-    scale: 1.0,
   };
 
   println!("dispatching...");
