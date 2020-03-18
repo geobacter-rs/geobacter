cc41544fd3923ed2dbcec4332159ed5799a3d0f0
Richard Diamond <wichard@vitalitystudios.com>
2020-03-17 20:46:41 -0500

    Handle late and zero sized allocations.

--- runtime-amd/src/alloc.rs
+++ runtime-amd/src/alloc.rs
@@ -34,6 +34,17 @@ impl AllocRef for LapAlloc {
     -> Result<NonNull<u8>, Self::Error>
   {
     let out_ptr = self.pool.alloc(layout)?;
+
+    if let Some(ref accessible) = self.accessible {
+      // Handle cases where `LapAllocAccess::add_access` is called before allocation.
+      // We don't need to check accessibility: it was already checked in
+      // LapAllocAccess::add_access.
+      let pool_ptr = unsafe {
+        MemoryPoolPtr::from_ptr(self.pool().clone(), out_ptr)
+      };
+      pool_ptr.grant_agents_access(&accessible)?;
+    }
+
     Ok(out_ptr)
   }
 
@@ -65,49 +76,58 @@ impl BuildAllocRef for LapAlloc {
 impl ReallocRef for LapAlloc { }
 impl Abort for LapAlloc { }
 
+#[doc(hidden)] #[inline(always)]
+fn add_access(build_alloc: &mut LapAlloc,
+              device: &HsaAmdGpuAccel,
+              pool_ptr: Option<MemoryPoolPtr<[u8]>>)
+  -> Result<(), HsaError>
+{
+  let already_accessible = build_alloc
+    .accessible
+    .iter()
+    .flat_map(|i| i.iter() )
+    .any(|a| a == device.agent() );
+  if already_accessible {
+    // nothing to do.
+    return Ok(());
+  }
+
+  // add this device to the accessible array: We need to do this now for later
+  // allocations (in the case of a LapVec).
+  if build_alloc.accessible.is_none() {
+    build_alloc.accessible = Some(Arc::default());
+  }
+  let accessible = Arc::make_mut(build_alloc.accessible.as_mut().unwrap());
+  accessible.push(device.agent().clone());
+
+  let pool = &build_alloc.pool;
+  let aa = pool.agent_access(device.agent())?;
+  if aa.never_allowed() {
+    error!("pool {:?} not sharable with this device ({:?})",
+           pool, device.id());
+    return Err(HsaError::General);
+  } else if aa.default_disallowed() {
+    if let Some(pool_ptr) = pool_ptr {
+      pool_ptr.grant_agents_access(&accessible)?;
+    }
+  } else if aa.default_allowed() {
+    // nothing to do
+  } else {
+    unreachable!("{:?}", aa);
+  }
+
+  Ok(())
+}
+
 /// No device removal.
-pub trait AllocAccess: BoxPoolPtr {
+pub trait LapAllocAccess: BoxPoolPtr {
   fn build_alloc_mut(&mut self) -> &mut LapAlloc;
 
   /// TODO: it would be nice to allow granting new devices access (which is expensive)
   /// concurrently. This is safe because only removal is concurrent-unsafe.
   fn add_access(&mut self, device: &HsaAmdGpuAccel) -> Result<(), HsaError> {
-    let already_accessible = self.build_alloc_mut()
-      .accessible
-      .iter()
-      .flat_map(|i| i.iter() )
-      .any(|a| a == device.agent() );
-    if already_accessible {
-      // nothing to do.
-      return Ok(());
-    }
-
-    let pool_ptr = unsafe { self.pool_ptr() };
-    if let None = pool_ptr { return Ok(()); }
-    let pool_ptr = pool_ptr.unwrap();
-    let pool = pool_ptr.pool();
-
-    let alloc = self.build_alloc_mut();
-    if alloc.accessible.is_none() {
-      alloc.accessible = Some(Arc::default());
-    }
-    let accessible = Arc::make_mut(alloc.accessible.as_mut().unwrap());
-    accessible.push(device.agent().clone());
-
-    let aa = pool.agent_access(device.agent())?;
-    if aa.never_allowed() {
-      error!("pool {:?} not sharable with this device ({:?})",
-             pool, device.id());
-      return Err(HsaError::General);
-    } else if aa.default_disallowed() {
-      pool_ptr.grant_agents_access(&accessible)?;
-    } else if aa.default_allowed() {
-      // nothing to do
-    } else {
-      unreachable!("{:?}", aa);
-    }
-
-    Ok(())
+    let ptr = unsafe { self.pool_ptr() };
+    add_access(self.build_alloc_mut(), device, ptr)
   }
 }
 
@@ -128,15 +148,71 @@ pub type LapVec<T> = alloc_wg::vec::Vec<T, LapAlloc>;
 /// Lap: LocallyAccessiblePool
 pub type LapBox<T> = alloc_wg::boxed::Box<T, LapAlloc>;
 
-impl<T> AllocAccess for LapVec<T> {
+impl<T> LapAllocAccess for LapVec<T> {
   fn build_alloc_mut(&mut self) -> &mut LapAlloc {
     self.build_alloc_mut()
   }
+
+  fn add_access(&mut self, device: &HsaAmdGpuAccel) -> Result<(), HsaError> {
+    if std::mem::size_of::<T>() == 0 {
+      // These are never actually allocated, so we can just say they're
+      // always accessible.
+      return Ok(());
+    }
+
+    let ptr = unsafe { self.pool_ptr() };
+    add_access(self.build_alloc_mut(), device, ptr)
+  }
 }
-impl<T> AllocAccess for LapBox<T>
+impl<T> LapAllocAccess for LapBox<T>
   where T: ?Sized,
 {
   fn build_alloc_mut(&mut self) -> &mut LapAlloc {
     self.build_alloc_mut()
+  }
+
+  fn add_access(&mut self, device: &HsaAmdGpuAccel) -> Result<(), HsaError> {
+    if std::mem::size_of_val(&**self) == 0 {
+      // These are never actually allocated, so we can just say they're
+      // always accessible: no delayed allocations for boxes.
+      return Ok(());
+    }
+
+    let ptr = unsafe { self.pool_ptr() };
+    add_access(self.build_alloc_mut(), device, ptr)
+  }
+}
+
+#[cfg(test)]
+mod test {
+  use super::*;
+  use crate::utils::test::*;
+
+  #[test]
+  fn lap_vec_late_alloc() {
+    let dev = device();
+
+    let mut m = LapVec::new_in(dev.fine_lap_node_alloc(0));
+    m.add_access(&dev).unwrap();
+
+    // now alloc:
+    m.resize(1, 0u32);
+
+    let accessible = m.build_alloc()
+      .accessible
+      .as_ref()
+      .unwrap()
+      .len();
+    assert_eq!(accessible, 1);
+  }
+
+  #[test]
+  fn lap_box_zero_sized_alloc() {
+    let dev = device();
+
+    let mut m = LapBox::new_in((), dev.fine_lap_node_alloc(0));
+    m.add_access(&dev).unwrap();
+
+    assert!(m.build_alloc().accessible.is_none());
   }
 }
